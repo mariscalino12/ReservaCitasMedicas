9b3c14a51a6b023eb52cd50ca04372e8dae00ec4
AlexHB <sirkrav18@gmail.com>
2025-06-17 22:04:21 -0500

    Se está subiendo las clases Entity

---
+++ src/main/java/com/elp/ReservasMedicas/Model/Medico.java
@@ -0,0 +1,106 @@
+package com.elp.ReservasMedicas.Model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "medicos")
+
+public class Medico {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_medico;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "apellido_paterno")
+	private String apellido_paterno;
+	
+	@Column(name = "apellido_materno")
+	private String apellido_materno;
+	
+	@Column(name = "numero_colegiado")
+	private Integer fecha_nacimiento;
+	
+	@Column(name = "horario_atencion")
+	private String horario_atencion;
+	
+	 @OneToMany(mappedBy = "medico")
+	    private List<Especialidad> especialidad;
+	 
+	 @OneToMany(mappedBy = "medico")
+	    private List<Cita> cita;
+
+	public Long getId_medico() {
+		return id_medico;
+	}
+
+	public void setId_medico(Long id_medico) {
+		this.id_medico = id_medico;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido_paterno() {
+		return apellido_paterno;
+	}
+
+	public void setApellido_paterno(String apellido_paterno) {
+		this.apellido_paterno = apellido_paterno;
+	}
+
+	public String getApellido_materno() {
+		return apellido_materno;
+	}
+
+	public void setApellido_materno(String apellido_materno) {
+		this.apellido_materno = apellido_materno;
+	}
+
+	public Integer getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(Integer fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+	public String getHorario_atencion() {
+		return horario_atencion;
+	}
+
+	public void setHorario_atencion(String horario_atencion) {
+		this.horario_atencion = horario_atencion;
+	}
+
+	public List<Especialidad> getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(List<Especialidad> especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public List<Cita> getCita() {
+		return cita;
+	}
+
+	public void setCita(List<Cita> cita) {
+		this.cita = cita;
+	}
+}
