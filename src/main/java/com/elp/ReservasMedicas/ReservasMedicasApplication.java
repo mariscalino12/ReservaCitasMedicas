7b602bce569814d087c5ba586e513da16ef1dc50
AlexHB <sirkrav18@gmail.com>
2025-06-17 21:30:24 -0500

    Proyecto Subido

---
+++ src/main/java/com/elp/ReservasMedicas/ReservasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.elp.ReservasMedicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elp/ReservasMedicas/ReservasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elp.ReservasMedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
